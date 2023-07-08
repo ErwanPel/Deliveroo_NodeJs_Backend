3ebfbbfe51a69007b095d3705e6ba1feccab51bf
Erwan <erwanpellerinpro@gmail.com>
2023-07-08 15:20:39 +0200

    create principal root and data json

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const data = require("./src/data/data.json");
+
+const app = express();
+app.use(express.json()).use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json(data);
+});
+
+app.all("*", (req, res) => {
+  try {
+    res.status(404).json({ message: "Page not Found !" });
+  } catch (error) {
+    res.status(500).json({
+      message:
+        "The application doesn't avaible for this moment, please retry later",
+    });
+  }
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Server has started 🚀");
+});
